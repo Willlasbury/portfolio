921079ccfb8dd98f0368a0d89251684bd761b844
Willlasbury <asburyw229@gmail.com>
2023-09-10 17:09:21 -0600

    work on adjusting styling:

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,11 +1,11 @@
 import './styles.css'
-
+import Nav from '../nav'
 export default function Header () {
 
     return (
         <header>
             <h1>Will Asbury</h1>
-
+            <Nav />
         </header>
     )
 }
--- src/components/nav/index.jsx
+++ src/components/nav/index.jsx
@@ -1,5 +1,13 @@
 import './styles.css'
 
 export default function Nav () {
+    return (
+    <nav>
+        <ul>
+            <li>About</li>
+            <li>Work</li>
+        </ul>
+    </nav>
 
+    )
 }
