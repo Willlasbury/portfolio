5ca8862b255a06bc66c4f317fbf2c4b0f0c89dbf
Willlasbury <asburyw229@gmail.com>
2023-09-19 11:30:17 -0700

    added logo links for github and Linkedin

---
+++ src/components/links/index.jsx
@@ -0,0 +1,22 @@
+import './styles.css'
+import gitLogo from '../../assets/logos/github-mark.png'
+import LinkLogo from '../../assets/logos/LI-In-Bug.png'
+
+
+export default function Nav () {
+    return (
+    <ul>
+        <li>
+            <a href='https://github.com/Willlasbury' target='_blank'> 
+                <img src={gitLogo} alt="Github Logo" />
+            </a>
+        </li>
+        <li>
+            <a href='https://www.linkedin.com/in/william-asbury-2613a824b/' target='_blank'> 
+                <img src={LinkLogo} alt='LinkedIn Logo'/>
+            </a>
+        </li>
+    </ul>
+
+    )
+}
