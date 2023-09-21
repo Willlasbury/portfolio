3f107f5da629a5c2118b71ce56818d6a83742ff6
Willlasbury <asburyw229@gmail.com>
2023-09-21 14:11:40 -0700

    added text and styling for about me

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -4,7 +4,7 @@ export default function Header () {
 
     return (
         <header>
-            <h1>Will Asbury</h1>
+            {/* <h1>Will Asbury</h1> */}
             <Links />
         </header>
     )
