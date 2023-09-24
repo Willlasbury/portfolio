dbfe58feea521c94f21edf1c129f01ab66949115
Willlasbury <asburyw229@gmail.com>
2023-09-24 11:22:13 -0700

    added project component for projects section

--- src/components/projects/index.jsx
+++ src/components/projects/index.jsx
@@ -1,10 +1,19 @@
 import "./styles.css";
 
+import Project from "../project";
+
 export default function Projects({ projects }) {
-  console.log("projects:", projects)
-  return (
-    <section>
-      <p>{projects ? "projects" : "nope"}</p>
-    </section>
-  );
+  if (projects) {
+    return (
+      <section>
+        {projects.map((project) => {
+          return (
+            <Project key={project.id} name={project.name} url={project.url} />
+          );
+        })}
+      </section>
+    );
+  } else {
+    return <p>Could not get projects from Github</p>;
+  }
 }
