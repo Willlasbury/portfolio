d572a5c90242fc3f780ec7243d602bad6ea58d98
Willlasbury <asburyw229@gmail.com>
2025-01-15 16:11:43 -0700

    adjusted code in get Code wars fetch

--- src/utils/api/getCodeWars/getCW.ts
+++ src/utils/api/getCodeWars/getCW.ts
@@ -1,15 +1,19 @@
-// import fetchStats from '../fetchUrl'
+export default async function getCW() {
+    try {
+        const url = 'https://www.codewars.com/api/v1/users/Willlasbury'
+        const res = await fetch(url);
 
-// export default async function () {
-//     try {
-//         const url = 'https://www.codewars.com/api/v1/users/Willlasbury'
-//         const service = 'codewars'
-//         const stats = await fetchStats(url, service);
-//         if (stats.message) {
-//           throw Error(stats.message);
-//         }
-//        return stats
-//       } catch (error) {
-//         return error;
-//       }
-// }
+        if (res.ok) {
+          const data = await res.json();
+          return data
+        } else {
+          throw new Error(`${res.statusText} ${await res.text()}`);
+        }
+      } catch (err) {
+        if (err instanceof Error) {
+          // TODO
+          console.log(err);
+        }
+        return [];
+      }
+    }
