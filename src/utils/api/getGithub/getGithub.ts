db72fb60f4ca5802e74f8973336b3eebd63fbbd9
Willlasbury <asburyw229@gmail.com>
2025-01-15 16:09:47 -0700

    adjusted get for github data

--- src/utils/api/getGithub/getGithub.ts
+++ src/utils/api/getGithub/getGithub.ts
@@ -1,10 +1,23 @@
-import fetchRepos from "../fetchUrl";
 import filterRepos from "./filterRepos";
 import Repository from "@/utils/types/githubReops";
 
 export default async function getGithub(): Promise<Array<Repository>> {
+  try {
     const url = "https://api.github.com/users/willlasbury/repos";
-    const service = 'github'
-    const repos: Array<Repository> = await fetchRepos(url, service);
-    return await filterRepos(repos); 
+
+    const res = await fetch(url);
+
+    if (res.ok) {
+      const data = await res.json();
+      return filterRepos(data);
+    } else {
+      throw new Error(`${res.statusText} ${await res.text()}`);
+    }
+  } catch (err) {
+    if (err instanceof Error) {
+      // TODO
+      console.log(err);
+    }
+    return [];
+  }
 }
