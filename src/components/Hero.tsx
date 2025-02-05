2562bb38c6da9ce34599a996e9a9957df4820280
Willlasbury <asburyw229@gmail.com>
2025-02-04 18:10:58 -0700

    added modal to hold about me

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,10 +1,12 @@
+import ModalPopup from "./ui/modal-popup"
+
 export default function Hero() {
   return (
     <section id="about" className="bg-gray-100 py-10">
       <div className="container mx-auto flex flex-col items-center">
         <h1 className="text-4xl font-bold mb-4">Welcome to My Portfolio</h1>
         <p className="text-xl mb-8">I'm a web developer passionate about creating amazing digital experiences.</p>
-        <button className="w-36 h-9">...</button>
+        <ModalPopup />
       </div>
     </section>
   )
