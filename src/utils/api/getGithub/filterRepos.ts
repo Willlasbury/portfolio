dbe1ebc51bdcb80ecd82a6d667b1b8a542085b4e
Willlasbury <asburyw229@gmail.com>
2025-02-10 12:47:05 -0700

    fixed certain usage in project cards

--- src/utils/api/getGithub/filterRepos.ts
+++ src/utils/api/getGithub/filterRepos.ts
@@ -1,10 +1,18 @@
 import Repository from "@/utils/types/githubRepos";
 
 export default function filterRepos(repos: Repository[]): Repository[] {
-    return repos.filter((repo) => {
-     const topics = repo.topics;
-     if (topics.includes("portfolio")) {
-       return repo;
-     }
-   });
- }
+  const inactive: Repository[] = []
+  const active: Repository[] = []
+  let i = 0;
+  while (i < repos.length) {
+    if (repos[i].topics.includes('inactive')) {
+      inactive.push(repos[i]);
+    }
+    if (repos[i].topics.includes('active')) {
+      active.push(repos[i]);
+    }
+    i++;
+
+  }
+  return active.concat(inactive);
+}
