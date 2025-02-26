310870d74ddd51ac19474564104e3b4292fecb5b
Willlasbury <asburyw229@gmail.com>
2025-02-26 14:05:43 -0700

    v1 radial chart

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css'
+import { useState, useEffect } from 'react'
 
 import Resume from './components/Resume'
 import { Carousel } from './components/carousel'
@@ -6,26 +7,38 @@ import Header from './components/Header'
 import Hero from './components/Hero'
 import Projects from './components/Projects'
 import Footer from './components/Footer'
+import StatsDisplay from './components/StatsDisplay'
+
 import getGithub from './utils/api/getGithub/getGithub'
-import { useState, useEffect } from 'react'
+import getCW from './utils/api/getCodeWars/getCW'
 import ProdRepository from './utils/types/prodRepo'
 import getRepoLanguages from './utils/api/getGithub/getRepoLangs'
+import UserStats from './utils/types/codeWarsData'
+import { storedCWdata } from './tests/mock/fakeData/cwData'
 
 export default function App() {
   const [projects, setProjects] = useState<ProdRepository[]>();
-  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [isLoadingProjects, setIsLoadingProjects] = useState<boolean>(true);
+  const [codeWarsData, setCodeWarsData] = useState<UserStats>(storedCWdata);
+  const [isLoadingCW, setIsLoadingCW] = useState(true);
 
   useEffect(() => {
-    const fetchRepoData = async () => {
+    const fetchData = async () => {
+      // get github repos
       const repos = await getGithub()
       for (let i = 0; i < repos.length; i++) {
         const data = await getRepoLanguages(repos[i].languages_url)
         repos[i] = { ...repos[i], languages: data } as ProdRepository
       }
       setProjects(repos as ProdRepository[])
-      setIsLoading(false)
+      setIsLoadingProjects(false)
+
+      const data = await getCW()
+      setCodeWarsData(data)
+      setIsLoadingCW(false)
+
     }
-     fetchRepoData()
+     fetchData()
   }, [])
 
 
@@ -36,7 +49,8 @@ export default function App() {
       <main className="flex flex-col flex-grow">
         <Hero />
         <Carousel>
-          <Projects projects={projects} isLoading={isLoading} />
+          <StatsDisplay data={codeWarsData} isLoadingCW={isLoadingCW} />
+          <Projects projects={projects} isLoadingProjects={isLoadingProjects} />
           <Resume />
         
       </Carousel>
---
+++ src/components/StatsDisplay.tsx
@@ -0,0 +1,38 @@
+import { GridLoader } from "react-spinners"
+import { Radar, RadarChart, PolarGrid, PolarAngleAxis, PolarRadiusAxis, ResponsiveContainer } from 'recharts';
+import UserStats from "../utils/types/codeWarsData";
+
+type StatsDisplayProps = {
+    data: UserStats
+    isLoadingCW: boolean
+}
+
+export default function StatsDisplay({ data, isLoadingCW }: StatsDisplayProps) {
+    const stats = Object.entries(data.ranks.languages)
+    const maxValue = Math.max(...stats.map(([_, obj]) => obj.rank * -1))
+    const minValue = Math.min(...stats.map(([_, obj]) => obj.rank * -1))
+    const formData = stats.map(([lang, obj]) => {
+        return { language: lang.toUpperCase(), rank: obj.rank * -1, score: obj.score }
+    })
+    console.log("maxValue, minValue:", maxValue, minValue)
+    console.log("maxValue-minValue:", maxValue-minValue)
+    return (
+        <section>
+            {isLoadingCW ? <GridLoader /> :
+                <ResponsiveContainer minWidth="500px" minHeight="500px" >
+                    <RadarChart cx="50%" cy="50%" outerRadius="80%" data={formData}>
+                        <PolarGrid />
+                        <PolarAngleAxis dataKey="language" />
+                        <PolarRadiusAxis
+                            angle={90}
+                            domain={[minValue, 8]}
+                            reversed={true}
+                            tickCount={maxValue - minValue + 2}
+                            />
+                        <Radar name="Mike" dataKey="rank" label={'rank'} stroke="#8884d8" fill="#8884d8" fillOpacity={0.6} />
+                    </RadarChart>
+                </ResponsiveContainer>
+            }
+        </section>
+    )
+}
