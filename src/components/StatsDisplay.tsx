eeefdeb6f422d245f6c8159907f58fdcd4048c52
Willlasbury <asburyw229@gmail.com>
2025-03-18 14:27:54 -0600

    added text labels to funnel graph

--- src/components/StatsDisplay.tsx
+++ src/components/StatsDisplay.tsx
@@ -1,7 +1,8 @@
 import { GridLoader } from "react-spinners"
 import { Radar, RadarChart, PolarGrid, PolarAngleAxis, PolarRadiusAxis, ResponsiveContainer } from 'recharts';
-import { Funnel, FunnelChart, LabelList, Tooltip } from 'recharts';
 import UserStats from "../utils/types/codeWarsData";
+import LeetCodeStats from "./dataCharts/leetCode"; 
+import CodeWarsStats from "./dataCharts/codeWars";
 
 type StatsDisplayProps = {
     data: UserStats
@@ -16,58 +17,16 @@ export default function StatsDisplay({ data, isLoadingCW, leetData }: StatsDispl
     const formData = stats.map(([lang, obj]) => {
         return { language: lang.toUpperCase(), rank: obj.rank * -1, score: obj.score }
     })
-    const colorHexCodes = [
-        "#B0BEC5", // Purple
-        "#81C784", // Green
-        "#FFEB3B", // Yellow
-        "#F44336"  // Red
-      ]
-    const displayedLeetData = leetData.data.matchedUser.submitStats.totalSubmissionNum
-    displayedLeetData.forEach((obj: any, i: number) => {
-        obj.fill = colorHexCodes[i]
-    })
+    
+
     return (
         <section className="flex flex-col justify-center items-center">
             <h2 className="h-1/6 text-3xl font-bold mb-2 text-center">Various User Stats</h2>
             {/* TODO: replace isloading stuff with <Suspense> https://react.dev/reference/react/Suspense */}
             {isLoadingCW ? <GridLoader /> :
                 <div className="flex justify-center items-center p-4 w-11/12 ">
-                    <ResponsiveContainer minWidth="100px" minHeight="260px">
-                        <FunnelChart width={730} height={250}>
-                            <Tooltip />
-                            <Funnel
-                                dataKey="count"
-                                data={displayedLeetData}
-                                isAnimationActive
-                            >
-                                <LabelList position="right" fill="#000" stroke="none" dataKey="difficulty" />
-                            </Funnel>
-                        </FunnelChart>
-                    </ResponsiveContainer>
-                    {/* <ResponsiveContainer minWidth="100px" minHeight="260px">
-                        <RadarChart outerRadius="80%" data={formData}>
-                            <PolarGrid />
-                            <PolarAngleAxis
-                                dataKey="language"
-                            //  dx={10}
-                            // dy={-10}
-                            />
-                            <PolarRadiusAxis
-                                angle={90}
-                                cx={-100}
-                                domain={[minValue, 8]}
-                                reversed={true}
-                                tickCount={maxValue - minValue + 2}
-                            />
-                            <Radar
-                                name="CodeWarsRadialGraph"
-                                dataKey="rank"
-                                label={'rank'}
-                                stroke="#8884d8"
-                                fill="#8884d8"
-                                fillOpacity={0.6} />
-                        </RadarChart>
-                    </ResponsiveContainer> */}
+                    <LeetCodeStats leetData={leetData} />
+                    <CodeWarsStats data={data} isLoadingCW={isLoadingCW} leetData={leetData} />
                 </div>
             }
         </section>
---
+++ src/components/dataCharts/leetCode.tsx
@@ -0,0 +1,71 @@
+import { ResponsiveContainer, Funnel, FunnelChart, Tooltip, TooltipProps, CartesianGrid, YAxis, LabelList } from 'recharts';
+import { NameType, ValueType } from 'recharts/types/component/DefaultTooltipContent';
+
+
+const CustomTooltip = ({ active, payload }: TooltipProps<ValueType, NameType>) => {
+    if (active && payload && payload.length) {
+        return (
+            <div className="custom-tooltip border border-gray-300 bg-white p-2 rounded-md shadow-xl">
+                <p className="label">{`${payload[0].payload.difficulty} Problems completed: ${payload[0].value}`}</p>
+            </div>
+        );
+    }
+}
+
+
+export default function LeetCodeStats({ leetData }: any) {
+
+    const colorHexCodes = [
+        "#B0BEC5", // Purple
+        "#81C784", // Green
+        "#FFEB3B", // Yellow
+        "#F44336"  // Red
+    ]
+    const displayedLeetData = leetData.data.matchedUser.submitStats.totalSubmissionNum.filter((obj: any, i: number) => {
+        obj.fill = colorHexCodes[i]
+        return obj.count != 0
+    })
+
+    const CustomLabel = (props: any) => {
+        const styleProps = {
+            fill: props.fill,
+            height: props.height,
+            textAnchor: 'middle',
+            width: props.width,
+            viewBox: props.viewBox,
+            x: props.x + props.width / 2,
+            y: props.y + props.height / 2,
+        };
+        
+        // Apparently the key can not be added through the spread operator
+        return (
+            <text key={props.index} {...styleProps}>
+                {props.value}
+            </text>
+        );
+    }
+    return (
+
+        <div className="flex flex-col justify-center items-center p-4 w-11/12 ">
+            <h3>Leet Code Stats</h3>
+            <ResponsiveContainer minWidth="100px" minHeight="260px">
+                <FunnelChart width={730} height={250}>
+                    <Tooltip content={<CustomTooltip />} />
+                    <Funnel
+                        dataKey="count"
+                        data={displayedLeetData}
+                        isAnimationActive
+
+                    >
+                        <LabelList dataKey="difficulty" position={'center'} fill='black'
+                         content={<CustomLabel />}
+                         />
+                        {/* <YAxis dataKey="difficulty" type="category" width={100} axisLine={false} tickLine={false} /> */}
+                    </Funnel>
+
+                </FunnelChart>
+            </ResponsiveContainer>
+        </div>
+
+    )
+}
